d69e9c8206096b954a2ae231330c696942599765
柴谷遼太朗 <shibataniryotaro@shibataniryoutarounoMacBook-Air.local>
2020-04-29 20:59:49 +0900

    modified Q11,Q13,Q15

--- task.rb
+++ task.rb
@@ -102,9 +102,7 @@ def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-  sports.flatten!
-  sports.uniq!
-  sports.each.with_index(1) do |sport,i|
+  sports.flatten!.uniq!.each.with_index(1) do |sport,i|
        puts "No#{i} #{sport}"
   end
 end
@@ -121,9 +119,7 @@ def q13
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
-  user_data[:age] = update_data[:age]
-  user_data[:address] = update_data[:address]
-  p user_data
+  p user_data.merge!(update_data)
 end
 
 def q14
@@ -138,17 +134,9 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-  if data1.key?(:age)
-    puts "OK"
-  else
-    puts "NG"
-  end
+  puts data1.key?(:age) ? "OK" : "NG"
 
-  if data2.key?(:age)
-    puts "OK"
-  else
-    puts "NG"
-  end
+  puts data2.key?(:age) ? "OK" : "NG"
 end
 
 def q16
